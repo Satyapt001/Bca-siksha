12994c78b7b1a9ce2f130a0c39bbd64079e38947
Satyapriya Tripathy <126075100+Satyapt001@users.noreply.github.com>
2024-10-27 20:31:47 +0530

    Update entry.js

--- user authentication/Bca-siksha login page/entry.js
+++ user authentication/Bca-siksha login page/entry.js
@@ -3,14 +3,14 @@ const signinForm = document.getElementById('signin-form');
 const switchToSignin = document.getElementById('switch-to-signin');
 const switchToSignup = document.getElementById('switch-to-signup');
 
+// Switch to Sign-Up form
+switchToSignup.addEventListener('click', function() {
+  signinForm.classList.add('hidden');
+  signupForm.classList.remove('hidden');
+});
 // Switch to Sign-In form
 switchToSignin.addEventListener('click', function() {
   signupForm.classList.add('hidden');
   signinForm.classList.remove('hidden');
 });
 
-// Switch to Sign-Up form
-switchToSignup.addEventListener('click', function() {
-  signinForm.classList.add('hidden');
-  signupForm.classList.remove('hidden');
-});
